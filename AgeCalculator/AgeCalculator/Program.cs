93ca054f6e76cdbb7fdbebfef9f9df97619f02be
Elizabeth Maraan <emaraan@gmail.com>
2016-01-12 15:55:31 -0800

    fixed part of math, was completed incorrectly at first, added timer

--- AgeCalculator/AgeCalculator/Program.cs
+++ AgeCalculator/AgeCalculator/Program.cs
@@ -8,24 +8,61 @@ namespace AgeCalculator
 {
     class Program
     {
+        private static System.Timers.Timer aTimer;
+        private static DateTime dob;
+        private static double daysInYear = 365.25;
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter your date of birth.");
-            var input = Console.ReadLine();
-            DateTime dob = DateTime.Parse(input);
-            DateTime currentTime = DateTime.Now;
-            var age = Math.Floor((currentTime.Subtract(dob).TotalDays)/365);
-            var ageHours = Math.Floor((currentTime.Subtract(dob).TotalHours));
-            var ageMinutes = Math.Floor((currentTime.Subtract(dob).TotalMinutes));
-            var ageSeconds = Math.Floor((currentTime.Subtract(dob).TotalSeconds));
-            var ageMilliSeconds = Math.Floor((currentTime.Subtract(dob).TotalMilliseconds));
-            Console.WriteLine("You are " + age + " years old.");
-            Console.WriteLine("You are " + ageHours + " hours old.");
-            Console.WriteLine("You are " + ageMinutes + " minutes old.");
-            Console.WriteLine("You are " + ageSeconds + " seconds old.");
-            Console.WriteLine("You are " + ageMilliSeconds + " milliseconds old.");
+            try
+            {
+                //Ask for date of birth and parse into DateTime
+                Console.WriteLine("What is your birthday? (Enter date in format MM/DD/YYYY):");
+                var input = Console.ReadLine();
+                dob = DateTime.Parse(input);
 
-            Console.ReadLine();
+                //Timer
+                aTimer = new System.Timers.Timer();
+                aTimer.Interval = 1000;
+                aTimer.Elapsed += OnTimedEvent;
+                aTimer.AutoReset = true;
+                aTimer.Enabled = true;
+
+                //Stop timer
+                Console.ReadLine();
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Please restart program and enter a valid date of birth.");
+               
+            }
+        }
+
+        private static void OnTimedEvent(Object source, System.Timers.ElapsedEventArgs e)
+        {
+            // Calculate how old the user is 
+            DateTime currentTime = DateTime.Now;
+            var ageYears = Math.Floor((currentTime.Subtract(dob).TotalDays) / 365.25);
+            var ageMonths = Math.Floor((currentTime.Subtract(dob).TotalDays)%365.25/30);
+            var ageWeeks = Math.Floor((currentTime.Subtract(dob).TotalDays)%365.25%30/7);
+            var ageDays = Math.Floor((currentTime.Subtract(dob).TotalDays) % 365.25 % 30 % 7);
+            var ageHours = Math.Floor((currentTime.Subtract(dob).TotalDays) % 365.25 % 30 % 7 / 24);
+            //var ageMinutes
+            //var ageSeconds
+            //var ageMilliSeconds
+            
+
+            // Tell user how old they are using different times 
+            Console.Clear();
+            Console.WriteLine("You are " + ageYears + " years, " + ageMonths + " months, " + ageWeeks + " weeks, " + ageDays +
+                " days old.");
+
+            /*Commented out because didn't do math right initially and will have to fix
+            
+            + ageHours + " hours, " + ageMinutes + " minutes, " + ageSeconds + 
+            " seconds, and " + ageMilliSeconds + " milliseconds old.");*/
+            Console.WriteLine("Press the enter key to exit the program at any time");
         }
     }
 }
